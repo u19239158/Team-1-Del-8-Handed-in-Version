13eb4bc3fc43593ff437cfec1674dfd4af662da8
u19239158 <62009940+u19239158@users.noreply.github.com>
2021-08-12 18:47:18 +0200

    Added the api to all the services

--- src/app/services/categorytype/categorytype.service.ts
+++ src/app/services/categorytype/categorytype.service.ts
@@ -1,67 +1,96 @@
 import { Injectable } from '@angular/core';
 import { last } from 'rxjs/operators';
 import { Categorytype } from 'src/app/interfaces';
+import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategorytypeService {
+  server = "https://localhost:44393/api/";
 
-  KEY = 'categorytypes';
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      ContentType: 'application/json'
+    })
+};
+  constructor(private http: HttpClient) { }
 
-  getAll(): Categorytype[] {
-    const categorytypes = JSON.parse(localStorage.getItem(this.KEY));
-
-    if(!categorytypes) {
-      const initialCategorytype: Categorytype = {
-        id: 1,
-        categoryType: 'small bolts',
-        productCategoryName: 'Product Category 1',
-      };
-      this.addCategorytype(initialCategorytype);
-      this.getAll();
-    }
-
-    return categorytypes;
+  GetCategoryType():  Observable<Categorytype[]>  {
+    return this.http.get<Categorytype[]>(`${this.server}Categorytype/GetCategoryType`).pipe(map(res => res));
   }
 
-  getCategorytypeById(id: number): Categorytype {
-    const categorytypes: Categorytype[] = JSON.parse(localStorage.getItem(this.KEY));
-    return categorytypes.find(x => x.id === id);
+  getCategoryTypeByID(categoryTypeid):  Observable<Categorytype>  {
+    return this.http.get<Categorytype>(`${this.server}Categorytype/GetCategoryTypeByID/${categoryTypeid}`).pipe(map(res => res));
   }
 
-  addCategorytype(newCategorytype: Categorytype): void {
-    const categorytypes: Categorytype[] = JSON.parse(localStorage.getItem(this.KEY));
-
-    if (!categorytypes) {
-      localStorage.setItem(this.KEY, JSON.stringify([newCategorytype]));
-      return;
-    }
-
-    let lastId = Math.max(...categorytypes.map(x => x.id));
-    newCategorytype.id = lastId++;
-    localStorage.setItem(this.KEY, JSON.stringify([...categorytypes, newCategorytype]));
+  CreateCategoryType(Categorytype:Categorytype):  Observable<Categorytype[]>  {
+    return this.http.post<Categorytype[]>(`${this.server}Categorytype/CreateCategoryType`, Categorytype,this.httpOptions);
   }
 
-  updateCategorytype(updated: Categorytype): void {
-    const categorytypes: Categorytype[] = JSON.parse(localStorage.getItem(this.KEY));
-    const index = categorytypes.findIndex(x => x.id == updated.id);
-
-    if(index > -1) {
-      categorytypes.splice(index, 1);
-      categorytypes.push(updated);
-      localStorage.setItem(this.KEY, JSON.stringify([...categorytypes]));
-    }
+  UpdateCategoryType(Categorytype:Categorytype):  Observable<Categorytype[]>  {
+    return this.http.put<Categorytype[]>(`${this.server}Categorytype/UpdateCategoryType`, Categorytype,this.httpOptions);
   }
 
-  deleteCategorytype(toDelete: Categorytype): void {
-    const categorytypes: Categorytype[] = JSON.parse(localStorage.getItem(this.KEY));
-    const index = categorytypes.findIndex(x => x.id == toDelete.id);
-
-    if(index > -1) {
-      categorytypes.splice(index, 1);
-      localStorage.setItem(this.KEY, JSON.stringify([...categorytypes]));
-    }
+    DeleteCategoryType(categoryTypeId):  Observable<Categorytype>  {
+    return this.http.delete<Categorytype>(`${this.server}Categorytype/DeleteCategoryType/${categoryTypeId}`).pipe(map(res => res));
   }
 }
+
+  // getAll(): Categorytype[] {
+  //   const categorytypes = JSON.parse(localStorage.getItem(this.KEY));
+
+  //   if(!categorytypes) {
+  //     const initialCategorytype: Categorytype = {
+  //       id: 1,
+  //       categoryType: 'small bolts',
+  //       productCategoryName: 'Product Category 1',
+  //     };
+  //     this.addCategorytype(initialCategorytype);
+  //     this.getAll();
+  //   }
+
+  //   return categorytypes;
+  // }
+
+  // getCategorytypeById(id: number): Categorytype {
+  //   const categorytypes: Categorytype[] = JSON.parse(localStorage.getItem(this.KEY));
+  //   return categorytypes.find(x => x.id === id);
+  // }
+
+  // addCategorytype(newCategorytype: Categorytype): void {
+  //   const categorytypes: Categorytype[] = JSON.parse(localStorage.getItem(this.KEY));
+
+  //   if (!categorytypes) {
+  //     localStorage.setItem(this.KEY, JSON.stringify([newCategorytype]));
+  //     return;
+  //   }
+
+  //   let lastId = Math.max(...categorytypes.map(x => x.id));
+  //   newCategorytype.id = lastId++;
+  //   localStorage.setItem(this.KEY, JSON.stringify([...categorytypes, newCategorytype]));
+  // }
+
+  // updateCategorytype(updated: Categorytype): void {
+  //   const categorytypes: Categorytype[] = JSON.parse(localStorage.getItem(this.KEY));
+  //   const index = categorytypes.findIndex(x => x.id == updated.id);
+
+  //   if(index > -1) {
+  //     categorytypes.splice(index, 1);
+  //     categorytypes.push(updated);
+  //     localStorage.setItem(this.KEY, JSON.stringify([...categorytypes]));
+  //   }
+  // }
+
+  // deleteCategorytype(toDelete: Categorytype): void {
+  //   const categorytypes: Categorytype[] = JSON.parse(localStorage.getItem(this.KEY));
+  //   const index = categorytypes.findIndex(x => x.id == toDelete.id);
+
+  //   if(index > -1) {
+  //     categorytypes.splice(index, 1);
+  //     localStorage.setItem(this.KEY, JSON.stringify([...categorytypes]));
+  //   }
+  // }
+
