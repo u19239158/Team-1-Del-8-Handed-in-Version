33e0c6c285daba614be9c50e7808febc5dd98dac
github_SonaliN18 <u19146851@tuks.co.za>
2021-08-22 01:04:28 +0200

    global error styling

--- src/app/admin/assign-delivery-order/assign-delivery-order.component.ts
+++ src/app/admin/assign-delivery-order/assign-delivery-order.component.ts
@@ -7,7 +7,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Deliveryshift } from 'src/app/interfaces';
 import { GlobalConfirmComponent } from 'src/app/modals/globals/global-confirm/global-confirm.component';
 import { DeliveryshiftService } from 'src/app/services/deliveryshift/deliveryshift.service';
-import {  HttpClient  } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-assign-delivery-order',
@@ -16,13 +16,14 @@ import {  HttpClient  } from '@angular/common/http';
 })
 export class AssignDeliveryOrderComponent implements OnInit {
 
-//search code
-Deliveryshifts: [];
-searchValue: string;
+  //search code
+  Deliveryshifts: Deliveryshift[];
+  searchValue: string;
+  dataNotFound: boolean;
 
   // unscheduleddeliverys: Deliveryshift[] = [];
-  Deliveryshift:Deliveryshift;
-  UnscheduledDelivery : Deliveryshift[];
+  Deliveryshift: Deliveryshift;
+  UnscheduledDelivery: Deliveryshift[];
   unscheduleddelivery: Observable<Deliveryshift[]>;
   dataSource = new MatTableDataSource<Deliveryshift>();
   displayedColumns: string[] = ['saleId', 'customerName', 'deliverydistance', 'orderAddress', 'deliverycourier'];
@@ -32,21 +33,35 @@ searchValue: string;
     private router: Router,
     private dialog: MatDialog,
     private httpClient: HttpClient
-    ) {}
+  ) { }
 
   ngOnInit(): void {
     this.readUnscheduledDeliveries();
 
-  this.DeliveryshiftService.GetUnscheduledDeliveries().subscribe((result:Deliveryshift[])=> {
-    this.UnscheduledDelivery = result; })
+    this.DeliveryshiftService.GetUnscheduledDeliveries().subscribe((result: Deliveryshift[]) => {
+      this.UnscheduledDelivery = result;
+    })
+  }
+
+  filter() {
+
+    const filter = (e) => {
+
+      return e.saleId && e.saleId.toLowerCase().includes(this.searchValue.toLowerCase()) ||
+        e.customerName && e.customerName.toLowerCase().includes(this.searchValue.toLowerCase())
+    }
+    // const data = (this.Deliveryshift.filter(filter))
+    // this.dataNotFound = data.length === 0
+    // this.dataSource = new MatTableDataSource(data)
+
   }
 
   readUnscheduledDeliveries(): void {
     //this.dataSource = new MatTableDataSource<UserRole[]>(this.UserRoleService.GetUserRole());
-     this.DeliveryshiftService.GetUnscheduledDeliveries().subscribe(res => {
-       console.log(res)
-       this.dataSource = new MatTableDataSource(res)
-     })
-    }
+    this.DeliveryshiftService.GetUnscheduledDeliveries().subscribe(res => {
+      console.log(res)
+      this.dataSource = new MatTableDataSource(res)
+    })
+  }
 
 }
