27076176b58493e4669d5167da34109fc7282500
github_SonaliN18 <u19146851@tuks.co.za>
2021-08-28 23:46:36 +0200

    PackOrder

--- src/app/admin/pack-order/pack-order.component.ts
+++ src/app/admin/pack-order/pack-order.component.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-enum CheckBoxType { AVAILABLE_FOR_COLLECTION, AVAILABLE_FOR_DELIVERY, NONE };
+enum CheckBoxType { READY_FOR_COLLECTION, READY_FOR_DELIVERY, NONE };
 
 @Component({
   selector: 'app-pack-order',
