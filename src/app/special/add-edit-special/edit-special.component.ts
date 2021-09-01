2745a2e2ff9ffaadddf1759924a120899c2fef5a
github_SonaliN18 <u19146851@tuks.co.za>
2021-09-01 17:58:16 +0200

    View sale, del shift, edit specials, place supplier order (with Sne)

--- src/app/special/add-edit-special/edit-special.component.ts
+++ src/app/special/add-edit-special/edit-special.component.ts
@@ -5,6 +5,7 @@ import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@ang
 import { ActivatedRoute, Router } from '@angular/router';
 import { Special, Productitem } from 'src/app/interfaces';
 import { HttpClient } from '@angular/common/http';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-edit-special',
@@ -13,35 +14,36 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EditSpecialComponent implements OnInit {
 
-    form: FormGroup;
-    id: number;
-    discountPercentage : number;
-    isAddMode: boolean;
-    loading = false;
-    submitted = false;
-    lls: Special;
-    productItem :Productitem;
-    specials: Observable<Special[]>;
-    collection = [];
-    selected : string;
+  form: FormGroup;
+  id: number;
+  discountPercentage: number;
+  isAddMode: boolean;
+  loading = false;
+  submitted = false;
+  special: Special = {} as Special;
+  // special: Special;
+  productItem: Productitem;
+  specials: Observable<Special[]>;
+  collection = [];
+  selected: string;
 
   constructor(
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-        private SpecialService: SpecialService,
-        private http : HttpClient
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private SpecialService: SpecialService,
+    private http: HttpClient
 
   ) { }
 
-  
+
   // ngOnInit(): void {
-    
+
   //   this.getCollection();
-    
+
   //   this.id = +this.route.snapshot.params['id'];
   //   this.SpecialService.getSpecialByID(this.id).subscribe(res =>{
-     
+
   //      this.special = res
   //     console.log(res)
   //   const formOptions: AbstractControlOptions = { };
@@ -52,27 +54,29 @@ export class EditSpecialComponent implements OnInit {
   //     specialStartDate: [this.special.specialStartDate, [Validators.required]],
   //     specialEndDate: [this.special.specialEndDate, [Validators.required]],
   //   }, formOptions); ;})
-    
+
   // }
 
   ngOnInit(): void {
     this.id = +this.route.snapshot.params['id'];
     this.getCollection();
-    const formOptions: AbstractControlOptions = { };
+    const formOptions: AbstractControlOptions = {};
     {
       this.SpecialService.getSpecialByID(this.id).subscribe(res => {
-        this.lls = res;
-       console.log(res)
+        this.special = res;
+        console.log(res)
         this.form = this.formBuilder.group({
-          id: [this.lls.specialID, Validators.required],
-          specialDescription: [this.lls.specialDescription, [Validators.required]],
-          discountId: [this.lls.discountId, [Validators.required]],
-          specialStartDate: [this.lls.specialStartDate, [Validators.required]],
-          specialEndDate: [this.lls.specialEndDate,[Validators.required]],
-    }, formOptions);
-    })
-  
-  }}
+          id: [this.special.specialID, Validators.required],
+          specialDescription: [this.special.specialDescription, [Validators.required]],
+          discountId: [this.special.discountId],
+          // moment(this.special.specialEndDate).format('YYYY-MM-DD'), [Validators.required],
+          specialStartDate: [moment(this.special.specialStartDate).format('YYYY-MM-DD'), [Validators.required]],
+          specialEndDate: [moment(this.special.specialEndDate).format('YYYY-MM-DD'), [Validators.required]],
+        }, formOptions);
+      })
+
+    }
+  }
 
 
 
@@ -84,7 +88,7 @@ export class EditSpecialComponent implements OnInit {
     this.loading = true;
     this.updateSpecial();
 
-  
+
     // if (this.isAddMode) {
     //     this.createSpecial();
     // } else {
@@ -96,10 +100,15 @@ export class EditSpecialComponent implements OnInit {
     this.http
       .get<any>('https://localhost:44393/api/Discount/GetDiscount').subscribe((res: any) => {
         this.collection = res;
+
+        setTimeout(() => {
+          // this.form.patchValue({ employeeId: 2 })
+        }, 3000);
+
       }, error => {
         console.log({ error });
       })
-      
+
   }
 
 
@@ -117,13 +126,13 @@ export class EditSpecialComponent implements OnInit {
 
   updateSpecial() {
     const special: Special = this.form.value;
-    special.specialID = this.lls.specialID;
-    special.productItemId = this.lls.productItemId;
-    special.productItemCost = this.lls.productItemCost;
+    special.specialID = this.special.specialID;
+    special.productItemId = this.special.productItemId;
+    special.productItemCost = this.special.productItemCost;
     this.SpecialService.UpdateSpecial(special).subscribe(res => {
       console.log(res)
       this.form.reset();
-    this.router.navigateByUrl('special');
+      this.router.navigateByUrl('special');
     });
   }
 
