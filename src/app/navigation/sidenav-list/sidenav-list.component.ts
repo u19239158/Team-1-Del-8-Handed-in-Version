f0fa77df9bff3f70998219899279f13c99e914c3
S'nethemba <u19239158@tuks.co.za>
2021-09-24 12:27:46 +0200

    Route to login

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -38,7 +38,7 @@ export class SidenavListComponent implements OnInit {
             });
   
     confirm.afterClosed().subscribe(res => {
-      this.router.navigateByUrl('login');
+      this.router.navigateByUrl('');
       this.onSidenavClose();
         })
       }
